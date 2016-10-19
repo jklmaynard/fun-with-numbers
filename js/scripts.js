d1e15f1ea42dff6c24392fd5b65b4843b1829e64
James Maynard <jameskirkmaynard@gmail.com>
2016-10-18 21:10:38 -0700

    Initial javascript functions to get the conjecture up and working, start on writing the website

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var collatz = function (num) {
+  if (num === 1) {
+    return console.log(num + ' is the loneliest number')
+  } else if (num % 2 === 0) {
+    return num/2
+  } else if (num % 2 !== 0) {
+    return (num*3)+1;
+  }
+};  
+var conjecture = function (num) {
+  var x = num
+  while (x !== 1) {
+    if (x === num) {
+      x = collatz(num)
+    } else {
+      x = collatz(x)
+    }
+    console.log(x);
+  }
+  console.log(x + ' is the lonelinest number');
+};
