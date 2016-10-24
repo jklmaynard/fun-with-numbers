bbdf120e064adda0bd78ee8c73f43b768286e73b
James Maynard <jameskirkmaynard@gmail.com>
2016-10-23 20:58:41 -0700

    Add all scrolling functionality, get some styles down, general functionality works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+var height = window.innerHeight;
 var collatz = function (num) {
   if (num === 1) {
     return
@@ -24,19 +25,33 @@ var conjecture = function (num) {
 };
 
 $(document).ready(function() {
-  $('.scroll').click(function(event) {
-    var y = $(window).scrollTop();
-    $(window).scrollTop(y + 800);
+  var num, y, arr, i;
+  // Reloading the page
+  $('.refresh-button').click(function() {
+    location.reload();
   });
+  // New page height
+  $('#intro').css('height', height);
+  $('#number').css('height', height);
+  setTimeout(function() {
+    if ($(window).scrollTop() > 0) {
+      $('body').animate({ scrollTop: $("#intro").offset().top }, 'slow');
+    }
+  }, 100);
+  // Click event for ready to calculate
+  $('.scroll').click(function(event) {
+    $('body').animate({ scrollTop: $("#number").offset().top }, 1000);
+  });
+  // Click event for calculation and population
   $('.collatz-number-scroll').click(function() {
-    var num, y, arr;
     num = $('#collatz-number').val();
     arr = conjecture(num);
-    y = $(window).scrollTop();
-    $(window).scrollTop(y + 800);
+    $('body').animate({ scrollTop: $("#conjectureflow1").offset().top }, 1000);
+    // Run thrugh array, adding a number div and appending to #conjecture-array-container
     arr.forEach(function(index) {
       var index_num = arr.indexOf(index);
-      $('#conjecture-array-container').append("<div class='col-md-1' id='array-index-number-" + index_num + "''>" + index + "</div>");
+      var el = "<div class='col-md-2 number-container' id='array-index-number-" + index_num + "''><h3>" + index + "</h3></div>"
+      $('#conjecture-array-container').append(el);
     });
   });
 });
